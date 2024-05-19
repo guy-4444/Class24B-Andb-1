c505e8ea85d6f056552730c50a062ee13857dc40
guy <guyskv@gmail.com>
2024-05-20 00:50:07 +0300

    Initial commit

---
+++ app/src/main/java/com/guy/class24b_andb_1/MainActivity.java
@@ -0,0 +1,41 @@
+package com.guy.class24b_andb_1;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextInputLayout textField;
+    private MaterialButton btn;
+    private boolean error = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        textField = findViewById(R.id.textField);
+//        btn = findViewById(R.id.btn);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toggleError();
+            }
+        });
+    }
+
+    private void toggleError() {
+        error = !error;
+        textField.setError(error ? "Invalid input" : "");
+    }
+}
